16628e869731128811c8309c0c56ac6ec3f464f2
carolchep <carolchepsam@gmail.com>
2021-12-13 12:48:21 +0300

    added Profile fo heroku

--- mul1/settings.py
+++ mul1/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w$de80p6aa#kq8141baz*0itllfjvbnax1lja-s!$87fk1kaj)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ 
+ALLOWED_HOSTS = ['127.0.0.1', 'crm.herokuapp.com']
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
